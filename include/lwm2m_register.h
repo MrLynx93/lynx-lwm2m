72356e08c6ed2a1305a71a98197fb2011b6b630c
MrLynx93 <mateusz.ziebura93@gmail.com>
2016-08-20 19:27:35 +0200

    Some of notification thread

--- include/lwm2m_register.h
+++ include/lwm2m_register.h
@@ -1,25 +1,10 @@
 #ifndef LYNX_LWM2M_LWM2M_REGISTER_H
 #define LYNX_LWM2M_LWM2M_REGISTER_H
 
-#include "lwm2m.h"
-#include "lwm2m_object.h"
-
-typedef struct lwm2m_register_message lwm2m_register_message;
-
-/*
-int on_registered(lwm2m_server* server, lwm2m_register_message *register_message); // (response????)
-int on_deregistered(lwm2m_server* server);*/
+#include "../include/lwm2m.h"
+#include "../include/lwm2m_object.h"
 
 /* Tries to register on all servers that has corresponding Server Object instance. Returns number of servers registered */
 int lwm2m_register_on_all_servers(lwm2m_context* context);
-
-struct lwm2m_register_message {
-    char* endpoint_client_name;
-    int lifetime;
-    char* lwm2m_version;
-    char* binding_mode;
-    int sms_number;
-    char* objects_and_instances;
-};
 
 #endif //LYNX_LWM2M_LWM2M_REGISTER_H
--- src/lwm2m_register.c
+++ src/lwm2m_register.c
@@ -4,12 +4,12 @@
 #include "../include/lwm2m_transport.h"
 
 
-int lwm2m_register_on_all_servers(lwm2m_context* context) {
-    lwm2m_map* server_instances = lwm2m_map_get(context->object_tree, SERVER_OBJECT_ID)->object.instances;
+int lwm2m_register_on_all_servers(lwm2m_context *context) {
+    lwm2m_map *server_instances = lwm2m_map_get(context->object_tree, SERVER_OBJECT_ID)->object.instances;
     int registered_servers = 0;
 
     int *instances_ids = (int *) malloc(sizeof(int) * server_instances->size);
-    for(int i = 0, instance_id = server_instances[i]; i < server_instances->size; i++) {
+    for (int i = 0, instance_id = server_instances[i]; i < server_instances->size; i++) {
         lwm2m_instance *instance = lwm2m_map_get(server_instances, instance_id)->instance;
         lwm2m_server *server = lwm2m_register(context, instance);
         if (server != NULL) {
@@ -20,14 +20,14 @@ int lwm2m_register_on_all_servers(lwm2m_context* context) {
 }
 
 
-lwm2m_server *lwm2m_register(lwm2m_context *context, lwm2m_instance *server_instance) {
+static lwm2m_server *lwm2m_register(lwm2m_context *context, lwm2m_instance *server_instance) {
     // Get necessary values
     lwm2m_resource *id_resource = lwm2m_map_get(instance->resources, SHORT_SERVER_ADDRESS_RESOURCE_ID)->resource;
     lwm2m_resource *lifetime_resource = lwm2m_map_get(server_instance->resources, LIFETIME_RESOURCE_ID)->resource;
     lwm2m_resource *binding_resource = lwm2m_map_get(server_instance->resources, BINDING_RESOURCE_ID)->resource;
 
     int short_server_id = id_resource.resource.single.value.int_value;
-    lwm2m_server_address *address = lwm2m_map_get(context->server_addresses,short_server_id);
+    lwm2m_server_address *address = lwm2m_map_get(context->server_addresses, short_server_id);
     char *endpoint_client_name = get_next_endpoint_client_name();
 
 
@@ -84,6 +84,6 @@ static char *create_registration_params(lwm2m_server *server) {
     return params;
 }
 
-static char* get_next_endpoint_client_name() {
+static char *get_next_endpoint_client_name() {
     return "abc";
 }
