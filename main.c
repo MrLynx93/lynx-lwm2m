a9c1c1567fce661f9b8ddf7a23104060e583de01
MrLynx <mateusz.ziebura93@gmail.com>
2016-09-28 22:43:14 +0200

    Created Cmake

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
-#include "include/lwm2m.h"
-
-
-lwm2m_object_tree* build_object_tree() {
-    lwm2m_create_object(1);
-
-
-}
+#include "lwm2m.h"
 
 
 int main() {
--- makefile
+++
@@ -1,6 +0,0 @@
-CFLAGS=-Wall
-SRC=$(src/*.c)
-HEADER=$(src/*.h)
-
-all:
-	gcc -std=c99 $(wildcard src/*.c) -I ./include -o lib.o
